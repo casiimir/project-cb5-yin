9aa7bb66375b735d2adc5aaae5e3099ed17affeb
Chiara Cirrito <chiara.cirrito1602@gmail.com>
2023-02-13 15:29:08 +0100

    feat: components

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from "./index.module.scss";
+
+const Footer = () => {
+  return (
+    <div className={styles.Footer}>
+      <p>QUI FOOTER</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import styles from "./index.module.scss";
+
+const Navbar = () => {
+  return (
+    <div className={styles.Navbar}>
+      <p>QUI NAVBAR</p>
+    </div>
+  );
+};
+
+export default Navbar;
