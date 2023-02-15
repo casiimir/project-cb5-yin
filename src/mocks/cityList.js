ccef09ccaf5c23d4b5be7f6f6d690cd16acb67e9
edwardsicily <98692976+edwardsicily@users.noreply.github.com>
2023-02-15 11:14:10 +0100

    fix: fixed Mockups

--- src/mocks/cityList.js
+++ src/mocks/cityList.js
@@ -1,11 +1,33 @@
 export const cityList = [
-  { id: 1, city: "Palermo" },
-  { id: 2, city: "Messina" },
-  { id: 3, city: "Agrigento" },
-  { id: 4, city: "Catania" },
-  { id: 5, city: "Ragusa" },
-  { id: 6, city: "Trapani" },
-  { id: 7, city: "Siracusa" },
-  { id: 8, city: "Caltanisetta" },
-  { id: 9, city: "Enna" },
+  {
+    name: "Palermo",
+    image_url:
+      "https://cf.bstatic.com/xdata/images/city/150x150/641173.jpg?k=4fa64534f0108918a6160c398e893dc73fffdb97ccde087424ed3143ccf92cf8&o=",
+    dest_id: "-123798",
+  },
+
+  {
+    name: "Agrigento",
+    image_url:
+      "https://cf.bstatic.com/xdata/images/city/150x150/639312.jpg?k=c7b60523232ec1f27fc913fb757205671fabf2bffb52c836df5892554b326e99&o=",
+    dest_id: "-110017",
+  },
+  {
+    name: "Trapani",
+    image_url:
+      "https://cf.bstatic.com/xdata/images/city/150x150/643396.jpg?k=0962b5df36da92fd5fc4b91ed86f37d4ac38f7b4f1a4ee55aea614baeae5d623&o=",
+    dest_id: "-131189",
+  },
+  {
+    name: "Siracusa",
+    image_url:
+      "https://cf.bstatic.com/xdata/images/city/150x150/642028.jpg?k=d9cde218450d3d4f7b8439537e68027fe91284869e810cc0c01bb6f788e46318&o=",
+    dest_id: "-129785",
+  },
+  {
+    name: "Catania",
+    dest_id: "-114787",
+    image_url:
+      "https://cf.bstatic.com/xdata/images/city/150x150/639757.jpg?k=0d0277cbc3fb5938af6888974e3c377568e825ef6aa87255501841a6b1ddc796&o=",
+  },
 ];
