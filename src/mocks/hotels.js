4f3d7e860c2db4b2532a666e77352e339628676e
Chiara Cirrito <chiara.cirrito1602@gmail.com>
2023-02-16 13:13:19 +0100

    Feat: mock Hotels

--- src/mocks/hotels.js
+++ src/mocks/hotels.js
@@ -1,20 +1,153 @@
 export const hotels = [
   {
-    id: 1,
-    name: "Hotel 1",
+    hotel_id: 1377073,
+    hoteltype_id: 204,
+    name: "Riu Plaza Berlin",
+    entrance_photo_url:
+      "https://cf.bstatic.com/images/hotel/square60/272/272369306.jpg",
+    review_score: "8.3",
+    countrycode: "de",
+    city: "Berlin",
+    country: "Germany",
+    class: 4,
+  },
+
+  {
+    hotel_id: 1377072,
+    hoteltype_id: 201,
+    name: "Apartment WILKY in Kaprun",
+    entrance_photo_url:
+      "https://cf.bstatic.com/images/hotel/square60/259/259695136.jpg",
+    review_score: "8.9",
+    countrycode: "at",
+    city: "Kaprun",
+    country: "Austria",
+    class: 0,
+  },
+
+  {
+    hotel_id: 1377071,
+    hoteltype_id: 204,
+    name: "HOF HOTEL",
+    entrance_photo_url:
+      "https://cf.bstatic.com/images/hotel/square60/342/342705900.jpg",
+    review_score: "8.8",
+    countrycode: "lt",
+    city: "Kaunas",
+    country: "Lithuania",
+    class: 4,
+  },
+
+  {
+    hotel_id: 1377070,
+    hoteltype_id: 201,
+    name: "Apartment Srblin",
+    entrance_photo_url:
+      "https://cf.bstatic.com/images/hotel/square60/105/105110522.jpg",
+    review_score: "8.4",
+    countrycode: "hr",
+    city: "Sveti Petar u Šumi",
+    country: "Croatia",
+    class: 2,
+  },
+
+  {
+    hotel_id: 1377066,
+    hoteltype_id: 222,
+    name: "English Romance Exclusive Apartments",
+    entrance_photo_url:
+      "https://cf.bstatic.com/images/hotel/square60/243/243472489.jpg",
+    review_score: "7.5",
+    countrycode: "de",
+    city: "Hannover",
+    country: "Germany",
+    class: 0,
+  },
+
+  {
+    hotel_id: 1377063,
+    hoteltype_id: 201,
+    name: "Apartment on Malysheva 84-5",
+    entrance_photo_url:
+      "https://cf.bstatic.com/images/hotel/square60/496/49670707.jpg",
+    review_score: "10.0",
+    countrycode: "ru",
+    city: "Yekaterinburg",
+    country: "Russia",
+    class: 0,
+  },
+
+  {
+    hotel_id: 1377059,
+    hoteltype_id: 220,
+    name: "Casa Bouganville",
+    entrance_photo_url:
+      "https://cf.bstatic.com/images/hotel/square60/472/47261874.jpg",
+    review_score: "8.4",
+    countrycode: "it",
+    city: "Positano",
+    country: "Italy",
+    class: 0,
   },
   {
-    id: 2,
-    name: "Hotel 2",
+    hotel_id: 1377057,
+    hoteltype_id: 208,
+    name: "b&b Due Papere",
+    entrance_photo_url:
+      "https://cf.bstatic.com/images/hotel/square60/406/406556550.jpg",
+    review_score: "10.0",
+    countrycode: "it",
+    city: "Casa Marchetti",
+    country: "Italia",
+    class: 0,
   },
-  { id: 3, name: "Hotel 3" },
-  { id: 4, name: "Hotel 4" },
-  { id: 5, name: "Hotel 5" },
-  { id: 6, name: "Hotel 6" },
-  { id: 7, name: "Hotel 7" },
-  { id: 8, name: "Hotel 8" },
-  { id: 9, name: "Hotel 9" },
-  { id: 10, name: "Hotel 10" },
-  { id: 11, name: "Hotel 11" },
-  { id: 12, name: "Hotel 12" },
+  {
+    hotel_id: 1377056,
+    hoteltype_id: 201,
+    name: "Guest House Mate",
+    entrance_photo_url:
+      "https://cf.bstatic.com/images/hotel/square60/132/132057885.jpg",
+    review_score: "9.6",
+    countrycode: "hr",
+    city: "Bol",
+    country: "Croatia",
+    class: 3,
+  },
+  {
+    hotel_id: 1377050,
+    hoteltype_id: 204,
+    name: "Gostinichny Kompleks Mashinostroeniya",
+    entrance_photo_url:
+      "https://cf.bstatic.com/images/hotel/square60/472/47261463.jpg",
+    review_score: "7.5",
+    countrycode: "ru",
+    city: "Reutov",
+    country: "Russia",
+    class: 0,
+  },
+
+  {
+    hotel_id: 1377044,
+    hoteltype_id: 212,
+    name: "Banksia Tourist Park",
+    entrance_photo_url:
+      "https://cf.bstatic.com/images/hotel/square60/255/255513750.jpg",
+    review_score: "7.9",
+    countrycode: "au",
+    city: "Perth",
+    country: "Australia",
+    class: 0,
+  },
+  {
+    hotel_id: 1377047,
+    hoteltype_id: 201,
+    name: "Apartment Ivan",
+    entrance_photo_url:
+      "https://cf.bstatic.com/images/hotel/square60/105/105100145.jpg",
+    review_score: "5.0",
+    countrycode: "hr",
+    city: "Novalja",
+    country: "Croatia",
+    class: 2,
+  },
 ];
