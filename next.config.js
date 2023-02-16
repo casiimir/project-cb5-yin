9090ff9f5208c4c1d3910afa32faa7f6cfdb730a
Hysen Shemollari <hysen.shemollari@gmail.com>
2023-02-16 18:57:05 +0100

    feat: CustomerReviews

--- next.config.js
+++ next.config.js
@@ -2,7 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["picsum.photos", "t-cf.bstatic.com"],
+    domains: [
+      "picsum.photos",
+      "graph.facebook.com",
+      "lh3.googleusercontent.com",
+      "cf.bstatic.com",
+      "lh5.googleusercontent.com",
+      "lh4.googleusercontent.com",
+      "t-cf.bstatic.com",
+    ],
   },
 };
 
--- src/components/guestReviews/GuestReviews.jsx
+++ src/components/guestReviews/GuestReviews.jsx
@@ -1,6 +1,14 @@
 import styles from "./index.module.scss";
 import Image from "next/image";
+import { customersReview } from "../../mocks/review_customers";
+import CardReviews from "./../CardReviews/CardReviews";
+
 const GuestReviews = () => {
+  //{
+  // customersReview.map((cust) => {
+  //   console.log(cust);
+  // });
+  //}
   return (
     <div className={styles.main}>
       <h2>Recensioni degli ospiti</h2>
@@ -9,23 +17,33 @@ const GuestReviews = () => {
         <h4>review_score_word</h4>
         <h6> n recensioni </h6>
       </div>
-      <div className={styles.container}>
-        <div className={styles.userReview}>
-          <Image
-            src="https://picsum.photos/200/300" //{entrance_photo_url}
-            alt="nameHotel" //{nameHotel}
-            width={40}
-            height={40}
-          />
-          <h2>Nome Utente</h2>
-        </div>
-        <div className={styles.cardReviews}>
-          <h3>Titolo Recensione</h3>
-          <p>Al momento non abbiamo recensioni</p>
-        </div>
-      </div>
+
+      {customersReview.map((customer) => {
+        return (
+          <div className={styles.container}>
+            <div className={styles.userReview}>
+              <Image
+                src={
+                  customer.author.avatar
+                    ? customer.author.avatar
+                    : "https://graph.facebook.com/v2.9/554753386/picture?type=square&height=64&width=64"
+                } //{entrance_photo_url}
+                alt="nameHotel" //{nameHotel}
+                width={40}
+                height={40}
+              />
+              <h2>{customer.title}</h2>
+            </div>
+            <div className={styles.cardReviews}>
+              <h3>{customer.title}</h3>
+              <p>{customer.cons}</p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
 
 export default GuestReviews;
+// <CardReviews key={customer.author.user_id} />
