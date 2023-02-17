674928b507e356d942093986d30f5545b9f877fa
FrancescoDeCaroCarella <francescodecarocarella@gmail.com>
2023-02-17 07:01:31 +0100

    Feat: Css City Gallery

--- src/mocks/cityList.js
+++ src/mocks/cityList.js
@@ -2,56 +2,56 @@ export const cityList = [
   {
     name: "Palermo",
     image_url:
-      "https://cf.bstatic.com/xdata/images/city/400x250/641173.jpg?k=4fa64534f0108918a6160c398e893dc73fffdb97ccde087424ed3143ccf92cf8&o=",
+      "https://cf.bstatic.com/xdata/images/city/1920x1080/641173.jpg?k=4fa64534f0108918a6160c398e893dc73fffdb97ccde087424ed3143ccf92cf8&o=",
     dest_id: "-123798",
   },
 
   {
     name: "Agrigento",
     image_url:
-      "https://cf.bstatic.com/xdata/images/city/300x250/639312.jpg?k=c7b60523232ec1f27fc913fb757205671fabf2bffb52c836df5892554b326e99&o=",
+      "https://cf.bstatic.com/xdata/images/city/1920x1080/639312.jpg?k=c7b60523232ec1f27fc913fb757205671fabf2bffb52c836df5892554b326e99&o=",
     dest_id: "-110017",
   },
   {
     name: "Trapani",
     image_url:
-      "https://cf.bstatic.com/xdata/images/city/300x250/643396.jpg?k=0962b5df36da92fd5fc4b91ed86f37d4ac38f7b4f1a4ee55aea614baeae5d623&o=",
+      "https://cf.bstatic.com/xdata/images/city/1920x1080/643396.jpg?k=0962b5df36da92fd5fc4b91ed86f37d4ac38f7b4f1a4ee55aea614baeae5d623&o=",
     dest_id: "-131189",
   },
   {
     name: "Siracusa",
     image_url:
-      "https://cf.bstatic.com/xdata/images/city/350x250/642028.jpg?k=d9cde218450d3d4f7b8439537e68027fe91284869e810cc0c01bb6f788e46318&o=",
+      "https://cf.bstatic.com/xdata/images/city/1920x1080/642028.jpg?k=d9cde218450d3d4f7b8439537e68027fe91284869e810cc0c01bb6f788e46318&o=",
     dest_id: "-129785",
   },
   {
     name: "Catania",
     dest_id: "-114787",
     image_url:
-      "https://cf.bstatic.com/xdata/images/city/300x250/639757.jpg?k=0d0277cbc3fb5938af6888974e3c377568e825ef6aa87255501841a6b1ddc796&o=",
+      "https://cf.bstatic.com/xdata/images/city/1920x1080/639757.jpg?k=0d0277cbc3fb5938af6888974e3c377568e825ef6aa87255501841a6b1ddc796&o=",
   },
   {
     name: "Messina",
     dest_id: "-121616",
     image_url:
-      "https://cf.bstatic.com/xdata/images/city/350x250/849898.jpg?k=5509e028df4af30bceba9201be37d0e765a9e75a5999204aa0330a86e91afaa1&o=",
+      "https://cf.bstatic.com/xdata/images/city/1920x1080/849898.jpg?k=5509e028df4af30bceba9201be37d0e765a9e75a5999204aa0330a86e91afaa1&o=",
   },
   {
     name: "Ragusa",
     dest_id: "-126066",
     image_url:
-      "https://cf.bstatic.com/xdata/images/city/400x250/636883.jpg?k=23b2f026fec68bb3184b537935917815913c474f5ca00384486ddf37a8441dd5&o=",
+      "https://cf.bstatic.com/xdata/images/city/1920x1080/636883.jpg?k=23b2f026fec68bb3184b537935917815913c474f5ca00384486ddf37a8441dd5&o=",
   },
   {
     name: "Enna",
     dest_id: "-117041",
     image_url:
-      "https://cf.bstatic.com/xdata/images/city/330x250/803284.jpg?k=23c7b48b3700c6e315a6121f4f1344d20a2e98a03709300b47077625dd5f9c58&o=",
+      "https://cf.bstatic.com/xdata/images/city/1920x1080/803284.jpg?k=23c7b48b3700c6e315a6121f4f1344d20a2e98a03709300b47077625dd5f9c58&o=",
   },
   {
     name: "Caltanissetta",
     dest_id: "-112596",
     image_url:
-      "https://cf.bstatic.com/xdata/images/city/270x250/885608.jpg?k=fce6f5378173fc4efe585f402ef58c65afded0622678600525c838886c4f3f2a&o=",
+      "https://upload.wikimedia.org/wikipedia/commons/9/96/Sant%27Agata_al_Collegio_Caltanissetta_2018.jpg",
   },
 ];
