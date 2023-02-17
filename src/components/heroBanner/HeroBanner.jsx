3e50d5e1707edcdee605847ba8e8a6c2d4f13194
FrancescoDeCaroCarella <francescodecarocarella@gmail.com>
2023-02-17 18:03:53 +0100

    Fix: MainLayout

--- src/components/heroBanner/HeroBanner.jsx
+++ src/components/heroBanner/HeroBanner.jsx
@@ -1,11 +1,12 @@
 import styles from "./index.module.scss";
 
-
 const HeroBanner = () => {
   return (
     <div className={styles.HeroBanner}>
-      <h1>Trova l'alloggio che fa per te</h1>
-      <h3>Cerca offerte su hotel, case e tanto altro...</h3>
+      <div className={styles.wrapperHeroBanner}>
+        <h1>Trova l'alloggio che fa per te</h1>
+        <h3>Cerca offerte su hotel, case e tanto altro...</h3>
+      </div>
     </div>
   );
 };
