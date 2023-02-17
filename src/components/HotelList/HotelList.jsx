c57fdbdd38eeaccb64f8bcb436d8cc899e3e141b
Hysen Shemollari <hysen.shemollari@gmail.com>
2023-02-17 16:13:39 +0100

    feat: DescriptionHotel

--- src/components/HotelList/HotelList.jsx
+++ src/components/HotelList/HotelList.jsx
@@ -1,48 +1,43 @@
 import Image from "next/image";
 import { hotels } from "@/mocks/hotels";
-import styles from "./index.module.scss"
-
-
+import styles from "./index.module.scss";
 
 const HotelList = () => {
-
-
-    return(
-       
-        <div className={styles.container}>
-        <h2>Chi ha guardato nome della struttura ha guardato anche queste strutture</h2>
-        <div className={styles.hotelContainer}>
-          {hotels.map((elem) => {
-            return (
-              <div className={styles.cardHotel}>
-                <img src={elem.entrance_photo_url} alt="img" />
-                <h3>{elem.name}</h3>
-                <p>
-                  punteggio, voto
-                </p>
-                <div className={styles.scoreContainer}>
-                    <div className={styles.wrap}>
-                        <p className={styles.review}>a partire da</p>
-                        <h3 className={styles.payment}>€ xxx</h3>
-                    </div>
-                  <button className={styles.btn}>Vedi disponibilità</button>
+  return (
+    <div className={styles.container}>
+      <h2>
+        Chi ha guardato nome della struttura ha guardato anche queste strutture
+      </h2>
+      <div className={styles.hotelContainer}>
+        {hotels.map((elem) => {
+          return (
+            <div className={styles.cardHotel} key={elem.hotel_id}>
+              <img src={elem.entrance_photo_url} alt="img" />
+              <h3>{elem.name}</h3>
+              <p>punteggio, voto</p>
+              <div className={styles.scoreContainer}>
+                <div className={styles.wrap}>
+                  <p className={styles.review}>a partire da</p>
+                  <h3 className={styles.payment}>€ xxx</h3>
                 </div>
+                <button className={styles.btn}>Vedi disponibilità</button>
               </div>
-            );
-          })}
-          <button className={styles.arrowRight}>
-            <Image
-              className={styles.svg}
-              src="/arrowright.svg"
-              alt=""
-              width={20}
-              height={20}
-              priority
-            />
-          </button>
-        </div>
+            </div>
+          );
+        })}
+        <button className={styles.arrowRight}>
+          <Image
+            className={styles.svg}
+            src="/arrowright.svg"
+            alt="image"
+            width={20}
+            height={20}
+            priority
+          />
+        </button>
       </div>
-    )
-}
+    </div>
+  );
+};
 
-export default HotelList
+export default HotelList;
--- src/components/hotelDescription/HotelDescription.jsx
+++ src/components/hotelDescription/HotelDescription.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import styles from "./index.module.scss";
 
-function HotelDescription() {
-  return <div>HotelDescription</div>;
+function HotelDescription({ descriptionData }) {
+  return (
+    <div className={styles.main}>
+      <h2>Il posto ideale per il tuo soggiorno</h2>
+      <p>{descriptionData?.description}</p>
+    </div>
+  );
 }
 
 export default HotelDescription;
