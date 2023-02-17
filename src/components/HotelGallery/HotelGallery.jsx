8dae1d02e1b3c01cfc06e26caded29cf3174797a
edwardsicily <98692976+edwardsicily@users.noreply.github.com>
2023-02-17 18:12:15 +0100

    feat: Hotel Gallery component

--- src/components/HotelGallery/HotelGallery.jsx
+++ src/components/HotelGallery/HotelGallery.jsx
@@ -1,8 +1,52 @@
-import React from "react";
+import { GET } from "@/utils/http";
+import React, { useEffect, useState } from "react";
 import styles from "./index.module.scss";
 
 function HotelGallery() {
-  return <div>HotelGallery</div>;
+  const [galleryData, setGalleryData] = useState(null);
+
+  useEffect(() => {
+    GET("hotels/photos?hotel_id=1377073&locale=it").then((res) =>
+      setGalleryData(res)
+    );
+  }, []);
+
+  if (!galleryData) return <div>ASPETTA</div>;
+  return (
+    <div className={styles.Gallery}>
+      <div className={styles.GalleryTop}>
+        <div className={styles.GalleryTopWrapper}>
+          <img
+            className={styles.GalleryWrapperTopImg}
+            src={galleryData[1].url_max}
+            alt=""
+          />
+          <img
+            className={styles.GalleryWrapperBottomImg}
+            src={galleryData[2].url_max}
+            alt=""
+          />
+        </div>
+        <div className={styles.GalleryTopImg}>
+          <img src={galleryData[0].url_max} alt={galleryData[0].tags[0].tag} />
+        </div>
+      </div>
+      <div className={styles.GalleryBottom}>
+        {galleryData.map((photo, id) => {
+          if (id < 3 || id > 7) return undefined;
+          return (
+            <div>
+              <img
+                className={styles.GalleryBottomImg}
+                src={photo.url_max}
+                alt={photo.tags[0].tag}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default HotelGallery;
