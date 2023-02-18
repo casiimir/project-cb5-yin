2f131c2595bfd263576c02d8e7cc5bc9bd9db314
edwardsicily <98692976+edwardsicily@users.noreply.github.com>
2023-02-18 12:31:56 +0100

    feat: gallery carousel placeholder and style

---
+++ src/components/hotelgalleryCarousel/HotelCarousel.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import styles from "./index.module.scss";
+
+function HotelCarousel({ galleryData }) {
+  return (
+    <div className={styles.HotelCarousel}>
+      <div className={styles.CarouselImgWrapper}>
+        <img
+          className={styles.CarouselImg}
+          src={galleryData[0].url_max}
+          alt=""
+        />
+        <p>ciao</p>
+      </div>
+    </div>
+  );
+}
+
+export default HotelCarousel;
