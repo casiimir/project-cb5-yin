0594f0ed1f8175c704d7cef504db170943cd2db5
Chiara Cirrito <chiara.cirrito1602@gmail.com>
2023-02-20 13:11:03 +0100

    Fix: about us

--- src/components/workTeam/WorkTeam.jsx
+++ src/components/workTeam/WorkTeam.jsx
@@ -1,5 +1,8 @@
+import { GrGithub, GrLinkedin } from "react-icons/gr";
+import { CgWebsite } from "react-icons/cg";
 import Image from "next/image";
 import styles from "./index.module.scss";
+import Link from "next/link";
 const WorkTeam = () => {
   return (
     <>
@@ -12,48 +15,103 @@ const WorkTeam = () => {
           </p>
         </div>
         <div className={styles.photos}>
-          <Image
-            src="https://picsum.photos/200/300"
-            alt="13"
-            width={40}
-            height={31}
-            priority
-          />
-          <Image
-            src="https://picsum.photos/200/300"
-            alt="13"
-            width={40}
-            height={31}
-            priority
-          />
-          <Image
-            src="https://picsum.photos/200/300"
-            alt="13"
-            width={40}
-            height={31}
-            priority
-          />
-          <Image
-            src="https://picsum.photos/200/300"
-            alt="13"
-            width={40}
-            height={31}
-            priority
-          />
-          <Image
-            src="https://picsum.photos/200/300"
-            alt="13"
-            width={40}
-            height={31}
-            priority
-          />
-          <Image
-            src="https://picsum.photos/200/300"
-            alt="13"
-            width={40}
-            height={31}
-            priority
-          />
+          <div className={styles.div}>
+            <Image
+              src="https://picsum.photos/200/300"
+              alt="13"
+              width={40}
+              height={31}
+              priority
+            />
+            <div className={styles.icons}>
+              <Link
+                href={"https://www.linkedin.com/in/chiara-cirrito-90a2021b7/"}
+              >
+                <GrLinkedin className={styles.icon} />
+              </Link>
+              <Link href={"https://github.com/chiacirrito"}>
+                <GrGithub />
+              </Link>
+              <Link
+                href={"https://chiacirrito.github.io/newportfolio/home.html"}
+              >
+                <CgWebsite />
+              </Link>
+            </div>
+          </div>
+          <div className={styles.div}>
+            <Image
+              src="https://picsum.photos/200/300"
+              alt="13"
+              width={40}
+              height={31}
+              priority
+            />
+            <div className={styles.icons}>
+              <Link href={"https://www.linkedin.com/in/edwardcatrimi/"}>
+                <GrLinkedin />
+              </Link>
+              <Link href={"https://github.com/edwardsicily"}>
+                <GrGithub />
+              </Link>
+            </div>
+          </div>
+          <div className={styles.div}>
+            <Image
+              src="https://picsum.photos/200/300"
+              alt="13"
+              width={40}
+              height={31}
+              priority
+            />
+            <div className={styles.icons}>
+              <Link
+                href={"https://www.linkedin.com/in/francescodecarocarella/"}
+              >
+                <GrLinkedin />
+              </Link>
+              <Link href={"https://github.com/FrancescoDeCaroCarella"}>
+                <GrGithub />
+              </Link>
+              <Link href={"https://francescodecarocarella.it/"}>
+                <CgWebsite />
+              </Link>
+            </div>
+          </div>
+          <div className={styles.div}>
+            <Image
+              src="https://picsum.photos/200/300"
+              alt="13"
+              width={40}
+              height={31}
+              priority
+            />
+            <div className={styles.icons}>
+              <Link href={"https://www.linkedin.com/in/gabriele-chiaramonte/"}>
+                <GrLinkedin />
+              </Link>
+              <Link href={"https://github.com/Gabriele9102"}>
+                <GrGithub />
+              </Link>
+            </div>
+          </div>
+          <div className={styles.div}>
+            <Image
+              src="https://picsum.photos/200/300"
+              alt="13"
+              width={40}
+              height={31}
+              priority
+            />
+            <div className={styles.icons}>
+              <Link href={"https://www.linkedin.com/in/hysen-shemollari/"}>
+                <GrLinkedin />
+              </Link>
+              <Link href={"https://github.com/ZaoTosh"}>
+                <GrGithub />
+              </Link>
+            </div>
+          </div>
         </div>
         <div className={styles.moreInfo}>
           <button type="button">Scopri di più</button>
