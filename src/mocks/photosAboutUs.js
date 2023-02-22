d3f6f842f5c109785a9995a2d0a26f9117e2c963
Gabriele <chiaramontegabriele4@gmail.com>
2023-02-22 18:26:13 +0100

    Fix: mock-Us

--- src/mocks/photosAboutUs.js
+++ src/mocks/photosAboutUs.js
@@ -1,48 +1,48 @@
 export const Us = [
   {
-    Name_first: "Chiara",
-    Photo_first:
+    Name: "Chiara",
+    Photo:
       "https://media.licdn.com/dms/image/D4D03AQF5j78_AcXHoQ/profile-displayphoto-shrink_400_400/0/1675946920826?e=1682553600&v=beta&t=pXI9H3jr86MFuXdW1oROpJnFBJrG6jX_oJp7uuQV3BM",
-    Linkedin_first: "https://www.linkedin.com/in/chiara-cirrito-90a2021b7/",
-    Github_first: "https://github.com/chiacirrito",
-    description_first:
+    Linkedin: "https://www.linkedin.com/in/chiara-cirrito-90a2021b7/",
+    Github: "https://github.com/chiacirrito",
+    description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas, omnis?",
   },
   {
-    Name_second: "Edward",
-    Photo_second:
+    Name: "Edward",
+    Photo:
       "https://media.licdn.com/dms/image/D4D03AQGMIsrxEhujpw/profile-displayphoto-shrink_400_400/0/1671562271918?e=1682553600&v=beta&t=bSH39MnQcRJ8qIjE4QrbIzUzOu6qDxzYFlT0EMJlP5w",
-    Linkedin_second: "https://www.linkedin.com/in/edwardcatrimi/",
-    Github_second: "https://github.com/edwardsicily",
-    description_second:
+    Linkedin: "https://www.linkedin.com/in/edwardcatrimi/",
+    Github: "https://github.com/edwardsicily",
+    description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas, omnis?",
   },
   {
-    Name_third: "Francesco",
-    Photo_third:
+    Name: "Francesco",
+    Photo:
       "https://media.licdn.com/dms/image/D4D03AQHrd-G0ilwgKg/profile-displayphoto-shrink_400_400/0/1663425606883?e=1682553600&v=beta&t=RmwLeN0fivVbqctC8SjqlSQlRdSNaglysA6Yy92NPmk",
-    Linkedin_third: "https://www.linkedin.com/in/francescodecarocarella/",
-    Github_third: "https://github.com/FrancescoDeCaroCarella",
-    Sito_third: "https://francescodecarocarella.it/",
-    description_third:
+    Linkedin: "https://www.linkedin.com/in/francescodecarocarella/",
+    Github: "https://github.com/FrancescoDeCaroCarella",
+    Sito: "https://francescodecarocarella.it/",
+    description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas, omnis?",
   },
   {
-    Name_fourth: "Gabriele",
-    Photo_fourth:
+    Name: "Gabriele",
+    Photo:
       "https://media.licdn.com/dms/image/D4D35AQHeH5oS-tddYg/profile-framedphoto-shrink_400_400/0/1675848640079?e=1677682800&v=beta&t=qaYEKOnUuCFApa9F_9bcM4pWQcUy27lDaRx4e9IKnLw",
-    Linkedin_fourth: "https://www.linkedin.com/in/gabriele-chiaramonte/",
-    Github_fourth: "https://github.com/Gabriele9102",
-    description_fourth:
+    Linkedin: "https://www.linkedin.com/in/gabriele-chiaramonte/",
+    Github: "https://github.com/Gabriele9102",
+    description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas, omnis?",
   },
   {
-    Name_fifth: "Zeni",
-    Photo_fifth:
+    Name: "Zeni",
+    Photo:
       "https://media.licdn.com/dms/image/D4E35AQH1fXi3h4Fuvw/profile-framedphoto-shrink_400_400/0/1676973857888?e=1677682800&v=beta&t=eWBfUe4M9E-wPI56qL5WIp0fyv_OdqC75gOwCXxd0PM",
-    Linkedin_fifth: "https://www.linkedin.com/in/hysen-shemollari/",
-    Github_fifth: "https://github.com/ZaoTosh",
-    description_fifth:
+    Linkedin: "https://www.linkedin.com/in/hysen-shemollari/",
+    Github: "https://github.com/ZaoTosh",
+    description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas, omnis?",
   },
 ];
