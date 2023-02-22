7436bb5c2dffd242f3cd53f2f68ea7b0e17c84e7
FrancescoDeCaroCarella <francescodecarocarella@gmail.com>
2023-02-22 12:17:41 +0100

    Feat: Css Hotel Page

--- src/components/hotelCarousel/HotelCarousel.jsx
+++ src/components/hotelCarousel/HotelCarousel.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import Image from "next/image";
 
 import "swiper/scss";
 import "swiper/scss/navigation";
@@ -10,7 +9,7 @@ import styles from "./index.module.scss";
 import { Navigation, Pagination } from "swiper";
 
 function HotelCarousel({ galleryData }) {
-  console.log(galleryData);
+  //console.log(galleryData);
   return (
     <div className={styles.HotelCarousel}>
       <Swiper
@@ -23,12 +22,9 @@ function HotelCarousel({ galleryData }) {
       >
         {galleryData.map((hotel) => {
           return (
-            <SwiperSlide
-              key={hotel?.photo_id}
-              className={styles.CarouselImgWrapper}
-            >
+            <SwiperSlide key={hotel?.photo_id} className={styles.slideWrapper}>
               <img
-                className={styles.CarouselImg}
+                className={styles.slideImg}
                 src={hotel.url_max}
                 alt={hotel?.ml_tags[0]?.tag_name}
               />
--- src/components/hotelDescription/HotelDescription.jsx
+++ src/components/hotelDescription/HotelDescription.jsx
@@ -2,10 +2,17 @@ import React from "react";
 import styles from "./index.module.scss";
 
 function HotelDescription({ descriptionData }) {
+
+  const handleClick = event => {
+  event.currentTarget.classList.toggle("line-clamp-5");
+
+  };
+
   return (
     <div className={styles.main}>
-      <h2>Il posto ideale per il tuo soggiorno</h2>
+      <h2>Descrizione della struttura</h2>
       <p>{descriptionData?.description}</p>
+            <button onClick={handleClick}>Leggi di più</button>
     </div>
   );
 }
