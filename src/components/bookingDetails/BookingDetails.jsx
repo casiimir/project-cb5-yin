b7078251afa2299c54a11ab305c210a54b0cc1fc
Chiara Cirrito <chiara.cirrito1602@gmail.com>
2023-02-22 14:57:31 +0100

    Feat:Reservation

--- src/components/bookingDetails/BookingDetails.jsx
+++ src/components/bookingDetails/BookingDetails.jsx
@@ -9,7 +9,7 @@ const BookingDetails = ({ results }) => {
           <p>Check-in</p>
           <h6> DATA-search </h6>
           <p>
-            {results.checkin.from}-{results.checkin.to}
+            {results?.checkin.from}-{results?.checkin.to}
           </p>
         </div>
 
@@ -17,13 +17,13 @@ const BookingDetails = ({ results }) => {
           <p>Check-out</p>
           <h6> DATA-search </h6>
           <p>
-            {results.checkout.from}-{results.checkout.to}
+            {results?.checkout.from}-{results?.checkout.to}
           </p>
         </div>
       </div>
 
       <div className={styles.stay}>
-        <p>Durata soggiorno</p>
+        <p>Durata soggiorno:</p>
         <h6> n. notti search</h6>
       </div>
     </div>
