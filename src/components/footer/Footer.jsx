70f3c397856f89ecc2cb98d24a726c47f1403f1f
FrancescoDeCaroCarella <francescodecarocarella@gmail.com>
2023-02-24 10:43:42 +0100

    Feat: Css MyWishPage

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className={styles.Footer}>
       <div className={styles.blue}>
-        <div className={styles.description}>
+        <div className={styles.content}>
           <h2>Risparmia tempo e denaro!</h2>
           <h3>Iscriviti e ti invieremo le migliori offerte</h3>
         </div>
@@ -17,15 +17,22 @@ const Footer = () => {
           <input className={styles.submit} type="submit" value="Iscriviti" />
         </form>
       </div>
-      <div className={styles.copyright}>
-        <p>Copyright © 1996–2023 Booking.com™. Tutti i diritti riservati.</p>
-        <p>
-          Booking.com è parte di Booking Holdings Inc., leader mondiale nel
-          settore dei viaggi online e relativi servizi.
-        </p>
-      </div>
-      <div className={styles.logos}>
-        <img className={styles.logo} width="130px" src="/booking.svg" alt="" />
+      <div className={styles.white}>
+        <div className={styles.copyright}>
+          <p>Copyright © 1996–2023 Booking.com™. Tutti i diritti riservati.</p>
+          <p>
+            Booking.com è parte di Booking Holdings Inc., leader mondiale nel
+            settore dei viaggi online e relativi servizi.
+          </p>
+        </div>
+        <div className={styles.logos}>
+          <img
+            className={styles.logo}
+            width="130px"
+            src="/booking.svg"
+            alt=""
+          />
+        </div>
       </div>
     </div>
   );
--- src/pages/yournexttrip/index.js
+++ src/pages/yournexttrip/index.js
@@ -4,8 +4,10 @@ import { getFavouritesFromLS } from "@/utils/utils";
 import styles from "@/styles/Favourites.module.scss";
 import Review from "@/atoms/Review/Review";
 import Image from "next/image";
+import Link from "next/link";
 import { useRouter } from "next/router";
-import Heart from "@/atoms/Heart/Heart";
+import CloseBtn from "@/atoms/CloseBtn/CloseBtn";
+import Button from "@/atoms/Button/Button";
 import AppContext from "@/store/context";
 
 function nextTrip() {
@@ -19,51 +21,115 @@ function nextTrip() {
   }, []);
   return (
     <MainLayout>
+      <div className={styles.headerWrapper}>
+        <div className={styles.headerContainer}>
+          <h1>Il tuo prossimo viaggio</h1>
+          <p>
+            <Image src={"heartred.svg"} width={14} height={14} alt="WishList" />{" "}
+            Le tue strutture salvate
+          </p>
+        </div>
+      </div>
       <div className={styles.FavouritesWrapper}>
         {!state.favourites || state.favourites.length === 0 ? (
-          <h2>No results</h2>
+          <>
+            <div className={styles.noFav}>
+              <div className={styles.icon}>
+                <Image
+                  src={"heart.svg"}
+                  width={40}
+                  height={40}
+                  alt="WishList"
+                />
+              </div>
+              <div className={styles.content}>
+                <h1>Salva e condividi le strutture che ti piacciono di più</h1>
+                <>
+                  <p>
+                    Grazie alle liste, puoi salvare le tue strutture preferite e
+                    condividerle con gli amici da qualunque dispositivo, quando
+                    vuoi.
+                  </p>
+                </>
+                <br></br>
+                <span>
+                  <h4>Comincia subito:</h4>
+                  <br></br>
+                  <ol>
+                    <li>
+                      Trova una struttura in cui ti piacerebbe soggiornare.
+                    </li>
+                    <li>Vai su “Aggiungi alla lista desideri”.</li>
+                    <li>
+                      Torna su questa pagina per consultare i tuoi preferiti
+                      ogni volta che vuoi.
+                    </li>
+                  </ol>
+                </span>
+                <Link href={"/"}>
+                  <Button
+                    style="submit"
+                    content="Trova la tua struttura ideale"
+                  />
+                </Link>
+              </div>
+            </div>
+          </>
         ) : (
           <>
-            {state.favourites?.map((el, idx) => (
-              <div key={idx} className={styles.Card}>
-                <div className={styles.ImageWrapper}>
-                  <Image
-                    src={el.main_photo_url.replace("square60", "square500")}
-                    alt="image"
-                    fill
-                    sizes="(max-width: 768px) 100vw,
+            <div className={styles.Fav}>
+              {state.favourites?.map((el, idx) => (
+                <div key={idx} className={styles.Card}>
+                  <div className={styles.ImageWrapper}>
+                    <Image
+                      src={el.main_photo_url.replace("square60", "square500")}
+                      alt="image"
+                      fill
+                      sizes="(max-width: 768px) 100vw,
               (max-width: 1200px) 50vw,
               33vw"
-                    priority
-                  />
-                </div>
-
-                <div className={styles.CardHeader}>
-                  <h3>{el.name}</h3>
-                  <Heart data={el} />
-                </div>
-                <div className={styles.Cardinfo}>
-                  <div className={styles.country}>
-                    <Image
-                      src={`https://flagcdn.com/${el?.countrycode}.svg`}
-                      width={20}
-                      height={15}
-                      alt="flag"
+                      priority
                     />
-
-                    <p>{el.country}</p>
                   </div>
-                  <p className={styles.address}>
-                    {el.city} {el.address}
-                  </p>
-                  <Review data={el} />
+                  <div className={styles.Cardinfo}>
+                    <div>
+                      <div className={styles.CardHeader}>
+                        <h3>{el.name}</h3>
+                        <CloseBtn data={el} />
+                      </div>
+                      <div className={styles.star}>⭐</div>
+                    </div>
+                    <div className={styles.review}>
+                      <Review data={el} />
+                    </div>
+                    <div className={styles.address}>
+                      <div className={styles.country}>
+                        <Image
+                          src={`https://flagcdn.com/${el?.countrycode}.svg`}
+                          width={25}
+                          height={20}
+                          alt="flag"
+                        />
+                        <span>
+                          <p>
+                            {el.country}, {el.city}
+                            <br></br>
+                            {el.address}
+                          </p>
+                        </span>
+                      </div>
+                    </div>
+                    <div className={styles.input}>
+                      <Button
+                        onClick={() => router.push(`hotel/${el.hotel_id}`)}
+                        style="submit"
+                        content="Vedi struttura"
+                      />
+                    </div>
+                  </div>
                 </div>
-
-                <button onClick={() => router.push(`hotel/${el.hotel_id}`)}>
-                  Vedi struttura
-                </button>
-              </div>
-            ))}
+              ))}
+            </div>
           </>
         )}
       </div>
