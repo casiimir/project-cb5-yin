ba311ebc1f95ed881ccbb1ad78fdabf35e73d6e7
Chiara Cirrito <chiara.cirrito1602@gmail.com>
2023-02-24 17:51:16 +0100

    Fix:About Us

--- src/components/aboutDescription/AboutDescription.jsx
+++ src/components/aboutDescription/AboutDescription.jsx
@@ -1,45 +1,54 @@
 import styles from "./index.module.scss";
 import OurTeam from "../OurTeam/OurTeam";
+import Image from "next/image";
+import Link from "next/link";
+
 const AboutDescription = () => {
   return (
     <div className={styles.main}>
       <div className={styles.container}>
         <div className={styles.title}>
-          <h1>Casimiro's Team</h1>
-          <p>the best Booking.com clone</p>
+          <h1>Il nostro progetto</h1>
+          <h4>Team Leader</h4>
+          <h4>Casimiro Cianciamino</h4>
+          <Image
+            src={
+              "https://media.licdn.com/dms/image/D4D03AQGANqMbwug4Cw/profile-displayphoto-shrink_400_400/0/1672573975735?e=1682553600&v=beta&t=yM7vY-YRTg0fIAvDoA8ui-gjUQFXY1PlKdywVzMGGyw"
+            }
+            className={styles.imgCasi}
+            alt={"img"}
+            width={150}
+            height={150}
+          />
         </div>
-
         <div className={styles.description}>
           <div className={styles.titleDescription}>
-            <h2>Project Description</h2>
+            <h2>Il miglior clone di Booking.com</h2>
           </div>
           <div className={styles.miniDescription}>
             <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Asperiores ab ad voluptatem quaerat illo veniam doloribus, hic
-              aspernatur unde, tenetur temporibus voluptate. Dolorem at
-              excepturi error! Odit nisi consequuntur doloremque veritatis in,
-              earum eius voluptas corporis debitis harum voluptatibus fuga
-              itaque aperiam tempore error? Asperiores perspiciatis vel,
-              similique doloribus eos ut, beatae accusamus minus excepturi
-              dolorem a vitae fugiat! Veritatis nesciunt officia aperiam quaerat
-              eum pariatur reprehenderit obcaecati quia ab sequi facere fugiat,
-              voluptatibus veniam, molestiae, officiis at. Eum, ullam. Unde
-              ullam, repellendus debitis voluptates labore praesentium fugiat
-              repudiandae officia in necessitatibus sapiente nesciunt esse
-              consequuntur temporibus minima amet cum voluptas pariatur
-              molestias nostrum exercitationem harum at nisi? Explicabo qui vel,
-              minus laboriosam ea iste veritatis provident voluptate praesentium
-              at adipisci autem odio, aliquam consequatur reprehenderit
-              repellat. Quidem consequuntur culpa vitae odio numquam doloribus
-              unde rerum ipsa aliquid veniam, eligendi qui repudiandae fugiat
-              aut eveniet! Delectus, a eveniet dolorem quaerat hic culpa impedit
-              quisquam optio repudiandae magni nesciunt suscipit sint fuga
-              error? Eaque quaerat nesciunt cum molestias modi harum eveniet nam
-              voluptate quas. Pariatur praesentium aut sapiente, esse nobis cum
-              aperiam temporibus impedit, fugiat facilis distinctio
-              reprehenderit ab nulla adipisci quasi eligendi magni voluptas
-              possimus animi quisquam. Ratione, dolore aliquid?{" "}
+              <Link
+                href={
+                  "https://www.booking.com/index.it.html?aid=376372&label=it-5Srxg0e1twJI_ryrey2UnQS267778030990%3Apl%3Ata%3Ap1%3Ap22.563.000%3Aac%3Aap%3Aneg%3Afi%3Atikwd-65526620%3Alp1008587%3Ali%3Adec%3Adm%3Appccp%3DUmFuZG9tSVYkc2RlIyh9YVcLb15uXY9drDRMbmnr9EE&sid=fcbb6f8c9ecce2c2826ce0a1d425d833&"
+                }
+              >
+                Booking.com
+              </Link>{" "}
+              è un'agenzia olandese, con sede ad Amsterdam, di viaggi online,
+              permette di prenotare alloggi e servizi aggiuntivi. Ci è stata
+              commissionata la realizzazione del clone perfetto del sito web{" "}
+              <Link
+                href={
+                  "https://www.booking.com/index.it.html?aid=376372&label=it-5Srxg0e1twJI_ryrey2UnQS267778030990%3Apl%3Ata%3Ap1%3Ap22.563.000%3Aac%3Aap%3Aneg%3Afi%3Atikwd-65526620%3Alp1008587%3Ali%3Adec%3Adm%3Appccp%3DUmFuZG9tSVYkc2RlIyh9YVcLb15uXY9drDRMbmnr9EE&sid=fcbb6f8c9ecce2c2826ce0a1d425d833&"
+                }
+              >
+                Booking.com
+              </Link>
+              , partendo dall'utilizzo di un'API denominata Booking on Rapid
+              API. Questo progetto è l'esame finale del programma di formazione
+              e sviluppo delle competenze del Coding Bootcamp di{" "}
+              <Link href={"https://edgemony.com"}>Edgemony.com</Link>,
+              un'importante scuola tecnologica di Palermo.
             </p>
           </div>
         </div>
